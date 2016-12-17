251bd87f3ca3f41a41c9d2eb180110de8b125ea6
GILE-16 <GILE-16>
2016-12-17 13:00:17 -0600

    First commit.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start:
+            Console.Write("Enter a value n:\nn = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci n = " + Fibulator(n));
+
+            goto Start;
+        }
+
+        static int Fibulator(int n)
+        {
+            return Fibulator(1, 1, n);
+        }
+
+        static int Fibulator(int a, int b, int n)
+        {
+            if(n<1)
+            {
+                return -1;
+            }
+            else if (n==1)
+            {
+                return a;
+            }
+            else if(n==2)
+            {
+                return b;
+            }
+            else if(n==3)
+            {
+                return a + b;
+            }
+            else
+            {
+                return Fibulator(b, a + b, n - 1);
+            }
+        }
+    }
+}
